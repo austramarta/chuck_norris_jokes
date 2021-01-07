f23648ec717ffc753afaf103e12c8cabe4cc8fd3
austramarta <austramarta@users.noreply.github.com>
2021-01-07 20:00:04 +0200

    chuck norris jokes

---
+++ app.js
@@ -0,0 +1,56 @@
+const button = document.querySelector(".get-jokes").addEventListener("click", generateJokes);
+
+function generateJokes(e) {
+
+    const number = document.querySelector("input[type=number]").value;
+    console.log(number);
+
+    //XMLHttpRequest
+    // const xhr = new XMLHttpRequest();
+    // xhr.open("GET", `http://api.icndb.com/jokes/random/${number}`, true);
+    // xhr.onload = function () {
+    //     if (this.status === 200) {
+    //         const response = JSON.parse(this.responseText);
+    //         console.log(response);
+    //         let output = "";
+    //         if (response.type === "success") {
+    //             for (joke of response.value) {
+    //                 output += `<li>${joke.joke}</li>`
+    //             }
+    //         } else {
+    //             output += '<li>Something went wrong</li>'
+    //         }
+    //         document.querySelector(".jokes").innerHTML = output;
+    //     }
+    // }
+    // xhr.send();
+
+
+    //FETCH
+    let output = "";
+
+    fetch(`http://api.icndb.com/jokes/random/${number}`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            // console.log(data);
+
+            let output = "";
+
+            for (joke of data.value) {
+                // console.log(joke.joke);
+                output += `<li> ${joke.joke}</li>`;
+            }
+
+            // const output = data.value.map(joke => `<li> ${joke.joke}</li>`).join("");
+
+            document.querySelector(".jokes").innerHTML = output;
+        })
+        .catch(function (err) {
+            console.log("Something went wrong!", err);
+        });
+
+    e.preventDefault();
+}
+
